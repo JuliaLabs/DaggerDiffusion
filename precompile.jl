eb548b2e439ed0299730116b4f2f72ded80b7866
Valentin Churavy <v.churavy@gmail.com>
2022-02-22 13:36:44 -0500

    update sysimage generation

--- precompile.jl
+++ precompile.jl
@@ -1,4 +1,5 @@
 using CairoMakie
+using Dagger
 
 T    = zeros(62, 62)
 node = Observable(T)
@@ -7,3 +8,5 @@ fig  = heatmap(node, colorrange = (0.0, 2.0))
 record(fig, "output.mkv", 1:1) do t
     node[] = T
 end
+
+fetch(Dagger.@spawn single=1 1)
--- sysimage.jl
+++ sysimage.jl
@@ -1,6 +1,10 @@
-using PackageCompiler
+#!/bin/env julia
+
 import Pkg
+Pkg.activate(;temp=true)
+Pkg.add("PackageCompiler")
+using PackageCompiler
 
 Pkg.activate(".")
 
-create_sysimage(["CairoMakie"]; sysimage_path="sysimage.so", precompile_execution_file="precompile.jl")
+create_sysimage(["CairoMakie", "Dagger"]; sysimage_path="sysimage.so", precompile_execution_file="precompile.jl")
